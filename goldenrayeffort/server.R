85f548434eadb3fbc54479690b4444d19f66153e
sntech-analytics <82458277+sntech-analytics@users.noreply.github.com>
2021-06-22 12:59:59 +0100

    Add files via upload

--- goldenrayeffort/server.R
+++ goldenrayeffort/server.R
@@ -1,5 +1,8 @@
+             
+              
 server <- function(input, output) {
-    
+
+library(config)    
 db_config <- config::get("dataconnection")
 con <- dbPool(
   drv = MariaDB(),
@@ -27,19 +30,63 @@ towtrack2 <- merge(greffort, towtrack2)
 towtrack2 <- towtrack2[c("Asset", "SampleID", "StartDate", "Date", "Latitude", "Longitude")]
 
 dfnephrops <- subset(dfcatch, dfcatch$Species %in% c('Nephrops', 'Nephrops Tails') & dfcatch$Asset=='GoldenRay')
-dfnephrops <- aggregate(RetainedWeight ~ Asset + SampleID, sum, data=dfnephrops)
+dfnephrops <- aggregate(RetainedWeight ~ Asset + SampleID + StartDateDT, sum, data=dfnephrops)
 dfnephrops <- merge(dfnephrops, dfeffort)
 dfnephrops$NephropsCPUE <- dfnephrops$RetainedWeight/dfnephrops$TowTimeHours
-dfnephrops <- dfnephrops[c("Asset", "SampleID", "NephropsCPUE", "RetainedWeight", "TowTimeHours")]
+dfnephrops <- dfnephrops[c("Asset", "SampleID", "StartDateDT", "NephropsCPUE", "RetainedWeight", "TowTimeHours")]
 
 combdat <- merge(towtrack2, dfnephrops, by=c("Asset", "SampleID"))
 combdat <- combdat[order(combdat$Date),] 
-    
+
+#For suncalc
+mindate <- min(dfnephrops$StartDateDT) - 15
+maxdate <- max(dfnephrops$StartDateDT) + 15
+medlat <- median(combdat$Latitude)
+medlong <- median(combdat$Longitude)
+moon <- getMoonIllumination(
+        date = seq.Date(mindate, maxdate, by = 1)) 
     
 pal <- colorNumeric(
   palette = "Reds",
   domain = combdat$NephropsCPUE)
 
+    
+#output$cpue<-renderPlotly({   
+output$cpue<-renderPlot({  
+    a <- ggplot(data=moon, aes(x=date, y=fraction)) +
+       theme_classic() +
+       ggtitle("Moon phase") +
+       labs(x = "Date", 
+            y = "Moon brightness") +
+       geom_line() +
+       scale_x_date(limits=c(mindate, maxdate)) +
+       theme(plot.title = element_text(face='bold', hjust=0.5),
+             axis.title.y  = element_text(face='bold'),
+             axis.title.x  = element_blank()
+        )
+
+
+    b <- ggplot(data=dfnephrops, aes(x=StartDateDT, y=NephropsCPUE, group=SampleID)) +
+       theme_classic() +
+       ggtitle("Nephrops CPUE") +
+       labs(x = "Date", 
+            y = "CPUE (kg/hour)") +
+       geom_bar(stat="identity", position='dodge', color='grey50') +
+       scale_x_date(limits=c(mindate, maxdate)) +
+       theme(legend.position = "none") +
+       theme(plot.title = element_text(face='bold', hjust=0.5),
+             axis.title.y  = element_text(face='bold'),
+             axis.title.x  = element_blank()
+        )
+    ggarrange(a, b, ncol = 1)
+#    fig1 <- ggplotly(a)
+#    fig2 <- ggplotly(b)
+#    fig <- subplot(fig1, fig2, nrows = 2)
+#    fig
+    })
+    
+    
+    
                     
 output$map <- renderLeaflet({
 #    maptow <- reactive({
@@ -87,6 +134,5 @@ observeEvent(input$newdates, {
     
     
  }
-
 
 
