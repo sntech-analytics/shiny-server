ea2344a93918fb641dc8ab6525b2f49f37729299
sntech-analytics <82458277+sntech-analytics@users.noreply.github.com>
2021-08-17 11:43:38 +0100

    Add files via upload

--- enkiplay/server.R
+++ enkiplay/server.R
@@ -3,11 +3,16 @@ library(plotly)
 library(scales)
 library(shinydashboard)
 library(shinyWidgets)
+library(wql)
+library(shiny)
+library(shinycssloaders)
 
 server <- function(input, output) {
 
 df <- readRDS("enki.RDS")
 
+df$salinityCorr <- ec2pss(df$salinity, df$temp, p = 0)
+df$salinityCorr2 <- ec2pss(df$salinity, df$temp, p = df$pressure*0.1)
 
 output$plots <- renderPlotly({
 grapha <- ggplot() +
@@ -20,15 +25,15 @@ grapha <- ggplot() +
       
 graphb <- ggplot() +
    theme_classic() +
-#   ggtitle("Salinity (ppt)") +
-   ylab("Salinity (ppt)") +
-   geom_line(data=df, aes(x=DateTime, y=salinity)) +
+   ylab("Salinity: To be calibrated") +
+#   geom_line(data=df, aes(x=DateTime, y=salinity)) +
+#   geom_line(data=df, aes(x=DateTime, y=salinityCorr)) +
+   geom_line(data=df, aes(x=DateTime, y=salinityCorr2)) +
    scale_x_datetime(labels = date_format("%Y-%m-%d %H"),
                      date_breaks = "1 hours")
       
 graphc <- ggplot() +
    theme_classic() +
-#   ggtitle("Temperature (°C)") +
    ylab("Temperature (°C)") +
    geom_line(data=df, aes(x=DateTime, y=temp)) +
    scale_x_datetime(labels = date_format("%Y-%m-%d %H"),
@@ -37,15 +42,85 @@ graphc <- ggplot() +
       
 graphd <- ggplot() +
    theme_classic() +
-#   ggtitle("Light (log(lux))") +
    ylab("Light (log(lux))") +
    geom_line(data=df, aes(x=DateTime, y=log10(lux))) +
    scale_x_datetime(labels = date_format("%Y-%m-%d %H"),
                      date_breaks = "1 hours")
 
-    subplot(ggplotly(grapha), ggplotly(graphb), ggplotly(graphc), ggplotly(graphd), nrows = 4,
+graphe <- ggplot() +
+   theme_classic() +
+   ylab("Conductance") +
+   geom_line(data=df, aes(x=DateTime, y=conduct)) +
+   scale_x_datetime(labels = date_format("%Y-%m-%d %H"),
+                     date_breaks = "1 hours")
+      
+
+    subplot(ggplotly(grapha), ggplotly(graphe), ggplotly(graphb), ggplotly(graphc), ggplotly(graphd), nrows = 5,
             titleY = TRUE, shareX = TRUE)
   })
+
+
+output$sumplotdepth <- renderPlotly({
+grapha <- ggplot() +
+   theme_classic() +
+   ylab("Temperature") +
+   xlab("Depth") +
+   geom_point(data=df, aes(y=temp, x=depthrev), color='black', size=2) +
+   geom_point(data=df[1:20,], aes(y=temp, x=depthrev), color='red', size=2) +
+   geom_point(data=df[1535:1570,], aes(y=temp, x=depthrev), color='blue', size=2) 
+   geom_point(data=df, aes(y=temp, x=depthrev))
+ 
+ graphb <- ggplot() +
+   theme_classic() +
+   ylab("Conductance") +
+   xlab("Depth") +
+   geom_point(data=df, aes(y=conduct, x=depthrev), color='black', size=2) +
+   geom_point(data=df[1:20,], aes(y=conduct, x=depthrev), color='red', size=2) +
+   geom_point(data=df[1535:1570,], aes(y=conduct, x=depthrev), color='blue', size=2) 
+   geom_point(data=df, aes(y=conduct, x=depthrev))
+      
+graphc <- ggplot() +
+   theme_classic() +
+   ylab("Salinity: To be calibrated") +
+   xlab("Depth") +
+   geom_point(data=df, aes(y=salinityCorr2, x=depthrev), color='black', size=2) +
+   geom_point(data=df[1:20,], aes(y=salinityCorr2, x=depthrev), color='red', size=2) +
+   geom_point(data=df[1535:1570,], aes(y=salinityCorr2, x=depthrev), color='blue', size=2) 
+   geom_point(data=df, aes(y=salinityCorr2, x=depthrev))
+ 
+    subplot(ggplotly(grapha), ggplotly(graphb), ggplotly(graphc), nrows = 3,
+            titleY = TRUE, shareX = TRUE)
+  })
+
+output$sumplottemp <- renderPlotly({
+grapha <- ggplot() +
+   theme_classic() +
+   ylab("Conductance") +
+   xlab("Temperature") +
+   geom_point(data=df, aes(y=conduct, x=temp), color='black', size=2) +
+   geom_point(data=df[1:20,], aes(y=conduct, x=temp), color='red', size=2) +
+   geom_point(data=df[1535:1570,], aes(y=conduct, x=temp), color='blue', size=2) 
+ 
+ graphb <- ggplot() +
+   theme_classic() +
+   ylab("Naive salinity measure") +
+   xlab("Temperature") +
+   geom_point(data=df, aes(y=salinity, x=temp), color='black', size=2) +
+   geom_point(data=df[1:20,], aes(y=salinity, x=temp), color='red', size=2) +
+   geom_point(data=df[1535:1570,], aes(y=salinity, x=temp), color='blue', size=2) 
+      
+graphc <- ggplot() +
+   theme_classic() +
+   ylab("Temperature-corrected salinity") +
+   xlab("Temperature") +
+   geom_point(data=df, aes(y=salinityCorr2, x=temp), color='black', size=2) +
+   geom_point(data=df[1:20,], aes(y=salinityCorr2, x=temp), color='red', size=2) +
+   geom_point(data=df[1535:1570,], aes(y=salinityCorr2, x=temp), color='blue', size=2) 
+ 
+    subplot(ggplotly(grapha), ggplotly(graphb), ggplotly(graphc), nrows = 3,
+            titleY = TRUE, shareX = TRUE)
+  })
+
 
 }
 
--- enkiplay/ui.R
+++ enkiplay/ui.R
@@ -3,12 +3,68 @@ library(plotly)
 library(scales)
 library(shinydashboard)
 library(shinyWidgets)
+library(shiny)
+library(shinycssloaders)
+
+#ui <- dashboardPage(
+#  dashboardHeader(title = "Enki play: Single trawl"),
+#  dashboardSidebar(),
+#  dashboardBody(plotlyOutput("plots", height = "80vh"))
+#)
+
+
+
+sidebar <-   dashboardSidebar(
+          tags$img(src="safetynet_logoWB.png",
+          title="SafetyNet Technologies",
+          width="230",
+          height="70"),
+        sidebarMenu(
+           menuItem("Enki traces", tabName = "traces", icon = icon("th")),          
+           menuItem("Exploratory graphs", tabName = "expgraphs", icon = icon("th"))
+        ),
+        tags$div(
+           tags$ul(
+            tags$li("The conductance should not be higher near the surface. The freshwater layer is on the top. So this must be a temperature artefact"),
+            tags$li("There is an issue with the conductance to salinity conversion. The Enki value is not corrected for temperature or 
+            pressure"),
+            tags$li("The naive salinity calculation is in the ballpark, but dependent on temperature. The new-improved calculation is off by about 10ppt."),
+            tags$li("There are equilibration issues as Enki descends (red symbols). We cannot reliably detect the thermocline (at about 20m) on descent. Maybe we can think about the smoothing 
+             rate a bit for the running average")
+              )
+           )            
+  )
+
+
+
+body <-   dashboardBody(
+    fluidRow(
+      tabItems(
+         tabItem(tabName = "traces",
+            h2("Data traces from a single haul", align = "center"),
+            h4("Click, hold, drag to select. Double click to revert", align = "center"),
+            shinycssloaders::withSpinner(
+              plotlyOutput("plots", height = "80vh"))
+            ),
+
+         tabItem(tabName = "expgraphs",
+            h2("Some exploratory bivariate relationships", align = "center"),
+            h4("Red = Enki descending; Blue = Enki ascending", align = "center"),
+            shinycssloaders::withSpinner(
+              box(plotlyOutput("sumplotdepth", height = "80vh"), width=6, height=800)),
+            shinycssloaders::withSpinner(
+              box(plotlyOutput("sumplottemp", height = "80vh"), width=6, height=800))              
+            )
+         )
+      )
+   )
+
+
 
 
 ui <- dashboardPage(
-  dashboardHeader(title = "Enki play: Single trawl"),
-  dashboardSidebar(),
-  dashboardBody(plotlyOutput("plots", height = "80vh"))
+  dashboardHeader(title = "Enki exploration"),
+  sidebar,
+  body
 )
-
 
